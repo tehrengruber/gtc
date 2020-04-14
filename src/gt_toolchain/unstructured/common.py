4782dc4bacb202321f7aa321c0293e84f60e6462
Hannes Vogt <hannes@havogt.de>
2020-04-14 11:28:17 +0200

    add missing file

---
+++ src/gt_toolchain/unstructured/common.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+#
+# Eve Toolchain - GT4Py Project - GridTools Framework
+#
+# Copyright (c) 2020, CSCS - Swiss National Supercomputing Center, ETH Zurich
+# All rights reserved.
+#
+# This file is part of the GT4Py project and the GridTools framework.
+# GT4Py is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or any later
+# version. See the LICENSE.txt file at the top-level directory of this
+# distribution for a copy of the license or check <https://www.gnu.org/licenses/>.
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import enum
+from eve.core import StrEnum
+
+
+class AssignmentKind(StrEnum):
+    """Kind of assignment: plain or combined with operations."""
+
+    PLAIN = "="
+    ADD = "+="
+    SUB = "-="
+    MUL = "*="
+    DIV = "/="
+
+
+@enum.unique
+class UnaryOperator(StrEnum):
+    """Kind of assignment: plain or combined with operations."""
+
+    POS = "+"
+    NEG = "-"
+
+
+@enum.unique
+class BinaryOperator(StrEnum):
+    """Binary operator identifier."""
+
+    ADD = "+"
+    SUB = "-"
+    MUL = "*"
+    DIV = "/"
+
+
+@enum.unique
+class DataType(enum.IntEnum):
+    """Data type identifier."""
+
+    INVALID = -1
+    AUTO = 0
+    BOOLEAN = 1
+    INT32 = 101
+    UINT32 = 102
+    FLOAT32 = 201
+    FLOAT64 = 202
+
+# TODO not really common
+@enum.unique
+class LoopOrder(enum.IntEnum):
+    """Loop order identifier."""
+
+    FORWARD = 1
+    BACKWARD = 2
+    # PARALLEL = 3
