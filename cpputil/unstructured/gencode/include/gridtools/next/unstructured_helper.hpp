792830815eeb50b5eb9e4a77b2e53bfb646ad0af
Hannes Vogt <hannes@havogt.de>
2020-07-03 15:28:42 +0000

    setup cpputil for gitpod

--- cpputil/unstructured/gencode/include/gridtools/next/unstructured_helper.hpp
+++ cpputil/unstructured/gencode/include/gridtools/next/unstructured_helper.hpp
@@ -7,8 +7,8 @@ namespace next {
 namespace mesh {
 
 // models hymaps as Mesh
-// TOOD(havogt): only if values model the Connectivity concept
-template <typename Key, typename Hymap> // TODO protect
+// TODO(havogt): protection: only if values model the Connectivity concept
+template <typename Key, typename Hymap> // TODO protect, only hymaps allowed
 decltype(auto) mesh_connectivity(const Hymap &mesh) {
   return at_key<Key>(mesh);
 }
