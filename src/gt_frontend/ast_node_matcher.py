cb2998c1a4b3ff70e272c89743b4d306e7f1e273
Till Ehrengruber <till.ehrengruber@cscs.ch>
2020-08-20 17:19:58 +0200

    Tests and fixes (default captures) for ast_node_matcher

--- src/gt_frontend/ast_node_matcher.py
+++ src/gt_frontend/ast_node_matcher.py
@@ -1,4 +1,5 @@
 from typing import Tuple, Dict, Any, List
+import copy
 import ast
 
 class Capture:
@@ -9,6 +10,35 @@ class Capture:
         self.name = name
         self.default = default
 
+# just some dummy classes for capturing defaults in lists
+class _Placeholder:
+    pass
+
+class _PlaceholderList(List):
+    pass
+
+class _PlaceholderAst(ast.AST):
+    pass
+
+def _get_placeholder_node(pattern_node):
+    if isinstance(pattern_node, List):
+        return _PlaceholderList()
+    elif isinstance(pattern_node, ast.AST):
+        return _PlaceholderAst()
+
+    return _Placeholder()
+
+def _is_placeholder_for(node, pattern_node):
+    """
+    Is the given node a valid placeholder for the pattern node
+    """
+    if isinstance(pattern_node, List) and isinstance(node, _PlaceholderList):
+        return True
+    elif isinstance(pattern_node, ast.AST) and isinstance(node, _PlaceholderAst):
+        return True
+
+    return False
+
 def check_optional(pattern_node, captures={}):
     """
     Check if the given pattern node is optional and populate the `captures` dict with the default values stored
@@ -16,13 +46,13 @@ def check_optional(pattern_node, captures={}):
     """
     if isinstance(pattern_node, Capture) and pattern_node.default != None:
         captures[pattern_node.name] = pattern_node.default
-        return True, captures
+        return True
     elif isinstance(pattern_node, ast.AST):
         is_optional = True
         for fieldname, child_node in ast.iter_fields(pattern_node):
-            is_optional &= check_optional(getattr(child_node, fieldname), captures)
-        return is_optional, captures
-    return False, captures
+            is_optional &= check_optional(child_node, captures)
+        return is_optional
+    return False
 
 def match(concrete_node, pattern_node, captures=None) -> Tuple[bool, Dict[str, ast.AST]]:
     if captures == None:
@@ -31,39 +61,38 @@ def match(concrete_node, pattern_node, captures=None) -> Tuple[bool, Dict[str, a
     if isinstance(pattern_node, Capture):
         captures[pattern_node.name] = concrete_node
         return True
-    elif type(concrete_node) != type(pattern_node):
+    elif type(concrete_node) != type(pattern_node) and not _is_placeholder_for(concrete_node, pattern_node):
         return False
     elif isinstance(pattern_node, ast.AST):
         # iterate over the fields of the concrete- and pattern-node side by side and check if they match
         for fieldname, pattern_val in ast.iter_fields(pattern_node):
-            if not hasattr(concrete_node, fieldname):
-                is_opt, opt_captures = check_optional(pattern_val)
+            if hasattr(concrete_node, fieldname):
+                if not match(getattr(concrete_node, fieldname), pattern_val, captures=captures):
+                    return False
+            else:
+                opt_captures = {}
+                is_opt = check_optional(pattern_val, opt_captures)
                 if is_opt:
                     # if the node is optional populate captures from the default values stored in the pattern node
                     captures.update(opt_captures)
                 else:
                     return False
-            else:
-                if not match(getattr(concrete_node, fieldname), pattern_val, captures=captures):
-                    return False
         return True
-    elif isinstance(concrete_node, List):
+    elif isinstance(pattern_node, List):
+        if not isinstance(concrete_node, List):
+            return False
+
+        if len(pattern_node) < len(concrete_node):
+            return False
+        elif len(pattern_node) > len(concrete_node):
+            # insert dummy nodes so that we can still call match on the pattern node and capture the defaults
+            concrete_node = [concrete_node[i] if i < len(concrete_node) else _get_placeholder_node(cpn) for i, cpn in enumerate(pattern_node)]
+
         return all([match(ccn, cpn, captures=captures) for ccn, cpn in zip(concrete_node, pattern_node)])
     elif concrete_node == pattern_node:
         return True
 
     return False
-
-# simple example
-#concrete_node = ast.With(items=["dummy"], body=["123"])
-#pattern_node = ast.With(items=Capture("items"), body=Capture("body"))
-
-#concrete_node = ast.With(items=["dummy"])
-#pattern_node = ast.With(items=Capture("items"), body=Capture("body", default=["abc"]))
-
-#matches, captures = match(concrete_node, pattern_node)
-
-#pass
 
 # todo: pattern node ast.Name(bla=123) matches ast.Name(id="123") since bla is not an attribute
 #  this can lead to errors which are hard to track
