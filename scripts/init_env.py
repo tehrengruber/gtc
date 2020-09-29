fcecd54e72d17e77f4559fd7c1cd6d3140729e7f
Enrique G. Paredes <enriqueg@cscs.ch>
2020-09-29 11:50:56 +0200

    Stabilize init_venv

--- scripts/init_env.py
+++ scripts/init_env.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import abc
 import argparse
 import os
 import re
 import subprocess
-from typing import Optional
+from typing import Dict, List, Optional, Type
 
 
 DEFAULT_MANAGER = "venv"
@@ -13,82 +14,85 @@ DEFAULT_NAME = "gtc"
 REPO_ROOT = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 
 
-class VenvManager:
-    @staticmethod
-    def create(py_version: str, venv_path: str) -> None:
-        cmd = f"python{py_version} -m venv {venv_path}"
+class VManager(abc.ABC):
+    @classmethod
+    def create(cls, py_version: str, venv_path: str) -> None:
+        cmds = cls.make_create_commands(py_version, venv_path)
         print(f"Creating virtual environment at '{os.path.abspath(venv_path)}' ...")
-        print(f"{cmd}")
+        cls.run_cmds(cmds)
+
+    @classmethod
+    def install(cls, requirements: str, venv_path: Optional[str] = None) -> None:
+        cmds = cls.make_install_commands(requirements, venv_path)
+        cls.run_cmds(cmds)
+
+    @classmethod
+    @abc.abstractmethod
+    def make_create_commands(cls, py_version: str, venv_path: str) -> List[str]:
+        ...
+
+    @classmethod
+    @abc.abstractmethod
+    def make_install_commands(cls, requirements: str, venv_path: Optional[str] = None) -> List[str]:
+        ...
+
+    @classmethod
+    def run_cmds(cls, cmds: List[str], exit_code: Optional[int] = -10) -> None:
         try:
-            subprocess.check_call(cmd.split())
-            cmd = f"{venv_path}/bin/pip3 install --upgrade wheel pip setuptools"
-            subprocess.check_call(cmd.split())
+            for cmd in cmds:
+                print(cmd)
+                subprocess.check_call(cmd.split())
         except subprocess.CalledProcessError as e:
             print(f"ERROR!\n{e}")
             exit(-10)
 
-    @staticmethod
-    def install(requirements: str, venv_path: Optional[str] = None) -> None:
+
+class VenvManager(VManager):
+    @classmethod
+    def make_create_commands(cls, py_version: str, venv_path: str) -> List[str]:
+        return [
+            f"python{py_version} -m venv {venv_path}",
+            f"{venv_path}/bin/pip3 install --upgrade wheel pip setuptools",
+        ]
+
+    @classmethod
+    def make_install_commands(cls, requirements: str, venv_path: Optional[str] = None) -> List[str]:
         pip_prefix = f"{venv_path}/bin/pip3" if venv_path else "pip3"
-        try:
-            if requirements == "run":
-                cmd = f"{pip_prefix} install -e {REPO_ROOT}[cpp]"
-            elif requirements == "develop":
-                cmd = (
-                    f"{pip_prefix} install -e {REPO_ROOT}[cpp] -r {REPO_ROOT}/requirements_dev.txt"
-                )
-            print(f"{cmd}")
-            subprocess.check_call(cmd.split())
-        except subprocess.CalledProcessError as e:
-            print(f"ERROR!\n{e}")
-            exit(-10)
+        if requirements == "run":
+            return [f"{pip_prefix} install -e {REPO_ROOT}[cpp]"]
+        elif requirements == "develop":
+            return [f"{pip_prefix} install -e {REPO_ROOT}[cpp] -r {REPO_ROOT}/requirements_dev.txt"]
+        else:
+            return []
 
 
-class VirtualenvManager:
-    @staticmethod
-    def create(py_version: str, venv_path: str) -> None:
-        cmd = f"virtualenv -p python{py_version} {venv_path}"
-        print(f"Creating virtual environment at '{os.path.abspath(venv_path)}' ...")
-        print(f"{cmd}")
-        try:
-            subprocess.check_call(cmd.split())
-        except subprocess.CalledProcessError as e:
-            print(f"ERROR!\n{e}")
-            exit(-10)
-
-    install = VenvManager.install
+class VirtualenvManager(VenvManager):
+    @classmethod
+    def make_create_commands(cls, py_version: str, venv_path: str) -> List[str]:
+        return [f"virtualenv -p python{py_version} {venv_path}"]
 
 
-class CondaManager:
-    @staticmethod
-    def create(py_version: str, venv_path: str) -> None:
-        cmd = f"conda create -p {venv_path} -y python={py_version}"
-        print(f"Creating virtual environment at '{os.path.abspath(venv_path)}' ...")
-        print(f"{cmd}")
-        try:
-            subprocess.check_call(cmd.split())
-        except subprocess.CalledProcessError as e:
-            print(f"ERROR!\n{e}")
-            exit(-10)
+class CondaManager(VManager):
+    @classmethod
+    def make_create_commands(cls, py_version: str, venv_path: str) -> List[str]:
+        return [f"conda create -p {venv_path} -y python={py_version}"]
 
-    @staticmethod
-    def install(requirements: str, venv_path: Optional[str] = None) -> None:
+    @classmethod
+    def make_install_commands(cls, requirements: str, venv_path: Optional[str] = None) -> List[str]:
         pip_prefix = f"conda run -p {venv_path} pip3" if venv_path else "pip3"
-        try:
-            if requirements == "run":
-                cmd = f"{pip_prefix} install -e {REPO_ROOT}[cpp]"
-            elif requirements == "develop":
-                cmd = (
-                    f"{pip_prefix} install -e {REPO_ROOT}[cpp] -r {REPO_ROOT}/requirements_dev.txt"
-                )
-            print(f"{cmd}")
-            subprocess.check_call(cmd.split())
-        except subprocess.CalledProcessError as e:
-            print(f"ERROR!\n{e}")
-            exit(-10)
+        if requirements == "run":
+            return [f"{pip_prefix} install -e {REPO_ROOT}[cpp]"]
+        elif requirements == "develop":
+            return [f"{pip_prefix} install -e {REPO_ROOT}[cpp] -r {REPO_ROOT}/requirements_dev.txt"]
+        else:
+            return []
 
 
-MANAGERS = {"venv": VenvManager, "virtualenv": VirtualenvManager, "conda": CondaManager}
+MANAGERS: Dict[str, Type[VManager]] = {
+    "venv": VenvManager,
+    "virtualenv": VirtualenvManager,
+    "conda": CondaManager,
+}
 
 
 # -- Parser
